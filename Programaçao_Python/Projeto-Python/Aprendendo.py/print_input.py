f2793ff39c04b8e25f7519385c487fa001c8c853
Rian Januario da Costa Santos <rianjcostasantos@gmail.com>
2024-09-09 12:47:32 -0300

    Desenvolvendo a linguagem python

---
+++ Programaçao_Python/Projeto-Python/Aprendendo.py/print_input.py
@@ -0,0 +1,8 @@
+nome = input("Report your name, please: ")
+idade = input("Report your age: ")
+
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep ="#")
